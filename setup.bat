c3e9daff337242247a105047f0a1e8b732988ebc
ouroborosno2 <764904497@qq.com>
2023-03-20 18:41:00 +0800

    进行了&和=的转义，但没有解决问题，因为实际上直接在cmd里对等号转义都没用

--- setup.bat
+++ setup.bat
@@ -4,8 +4,21 @@
 ::读取配置文件base_folder字段
 for /f %%t in ('call bin/myUtils func_getPara base_folder') do (set base_folder=%%~ft)
 if "%base_folder%"=="""" (echo please input [base_folder] & goto:end)
-::遇到&需要转义！！,cmd的转义符是^,并且要把转义符本身添加到变量里，这样执行命令时才不会受影响
-set base_folder=%base_folder:&=^^^&%
+::遇到&,等号需要转义，并且顺序不能错！等号转义难，先转井号再换转义符,cmd的转义符是^,并且要把转义符本身添加到变量里，这样执行命令时才不会受影响
+set base_folder=%base_folder:&=SPECIAL_AND%
+    set CURSTR=%base_folder%
+    set RESULT=
+    :cicleNext
+    if "%CURSTR%" equ "" goto cicleEnd
+    set a=%CURSTR:~0,1%
+    if "%a%" equ "=" (set RESULT=%RESULT%SPECIAL_EQUATION) else (set RESULT=%RESULT%%a%)
+    set CURSTR=%CURSTR:~1%
+    goto cicleNext
+    :cicleEnd
+set base_folder=%RESULT:SPECIAL_EQUATION=^^^=%
+set base_folder=%base_folder:SPECIAL_AND=^^^&%
+echo %base_folder%
+
 ::echo %base_folder%
 
 ::先创建文件夹，再批量移动
